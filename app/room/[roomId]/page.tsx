0f0873fc310c6d4c008530fe212fd220bb9ab70f
atsuatsu0120 <masa_yuka_atu0120@yahoo.co.jp>
2023-12-13 13:58:10 +0900

    Page to App

--- app/room/[roomId]/page.tsx
+++ app/room/[roomId]/page.tsx
@@ -1,8 +1,81 @@
-export default function Page({params}: {params: {roomId: string}}) {
-    return (
-        <div>
-            <h1>Room {params.roomId}</h1>
-        </div>
-    )
+import { NextPage } from "next";
+import { Header } from "@/ui/Header";
+import { BingoCard } from "@/ui/BingoCard";
+import { Alert, AlertIcon, Box, Center, Container, Text, useControllableState, useDisclosure } from "@chakra-ui/react";
+import Head from "next/head";
+import { BingoAccountModal } from "@/ui/BingoAccountModal";
+import { BingoModal, BingoModalType, BingoModalTypes } from "@/ui/BingoModal";
+import { useContext, useEffect } from "react";
+// import { BingoContext, BingoProvider } from "../src/contexts/bingo-context";
+// import { SocketIOContext, SocketIOProvider } from "../src/contexts/socketio-context";
+// import useSound from "use-sound";
 
+const BingoContent: React.FC = () => {
+    // const {socketio} = useContext(SocketIOContext);
+    // const {name} = useContext(BingoContext);
+    const bingoModal = useDisclosure();
+    const [ bingoModalType, setBingoModalType ] = useControllableState<BingoModalType>({defaultValue: BingoModalTypes.Bingo});
+    // const [ activePrizeResult, setActivePrizeResult ] = useControllableState<PrizeResultMessage>({defaultValue: null!});
+    const [play] = useSound("../public/Decision.wav");
+
+    // useEffect(() => {
+    //     const handleBingo = (obj: BingoMessage) => {
+    //         play();
+    //         console.log(obj);
+    //         setBingoModalType(BingoModalTypes.Bingo);
+    //         bingoModal.onOpen();
+    //     }
+    //     const handlePrizeResult = (obj: PrizeResultMessage) => {
+    //         console.log(obj);
+    //         setTimeout(() => {
+    //             setActivePrizeResult(obj);
+    //             setBingoModalType(BingoModalTypes.Result);
+    //         }, 2000);
+    //     }
+    //     // socketio.on("bingo", handleBingo);
+    //     // socketio.on("prizeResult", handlePrizeResult);
+
+    //     return () => {
+    //         // socketio.off("bingo", handleBingo);
+    //         // socketio.off("prizeResult", handlePrizeResult);
+    //     }
+    // }, [socketio, bingoModal, setBingoModalType, setActivePrizeResult, play]);
+
+    return (
+        <Center flexDirection="column">
+            <Box p="24px">
+                <Text fontSize="lg" fontWeight="bold">{"山口" /*name*/} さんのビンゴカード</Text>
+            </Box>
+            <BingoCard />
+            <Box w="full" mt="12px">
+                <Alert status="info" bg="#E2E8F0" w="full" borderRadius="6px">
+                    <AlertIcon color="#718096" />
+                    参加中：*人
+                </Alert>
+            </Box>
+            <BingoModal type={bingoModalType} prizeResult={/*activePrizeResult*/"10"} isOpen={bingoModal.isOpen} onClose={bingoModal.onClose} onOpen={bingoModal.onOpen} setBingoModalType={setBingoModalType} />
+        </Center>
+    );
 }
+
+const BingoPage: NextPage = () => {
+    return (
+        <>
+        {/*<SocketIOProvider>*/}
+            {/* <BingoProvider> */}
+                <Head>
+                    <title>Bingo</title>
+                </Head>
+                <Header />
+                <Box as="main">
+                    <Container>
+                        <BingoContent />
+                    </Container>
+                </Box>
+                <BingoAccountModal />
+            {/* </BingoProvider> */}
+        {/*</SocketIOProvider> */}
+        </>
+    );
+}
+export default BingoPage;
---
+++ ui/BingoNumber.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { NumberIcon, NumberIconProps } from "@/ui/NumberIcon";
+
+type BingoNumberProps = {
+    type: "normalNumber" | "hitNumber";
+    bingoNumber: number;
+}
+type BingoCenterProps = {
+    type: "center";
+}
+type Props = BingoNumberProps | BingoCenterProps;
+
+const isBingoCenter = (props: Props): props is BingoCenterProps => props.type === "center";
+
+export function BingoNumber(props: Props) {
+    return (
+        <NumberIcon bg={props.type === "hitNumber" ? "#319795" : props.type === "center" ? "#D69E2E" : "#EDF2F7"}
+          color={props.type !== "normalNumber" ? "#fff" : "#000"}>
+            {
+                isBingoCenter(props) ? "＊" : props.bingoNumber
+            }
+        </NumberIcon>
+    )
+};
